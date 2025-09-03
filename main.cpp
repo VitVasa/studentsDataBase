dfab09e1ec15215d21e53ab9886e429ede0ce934
VitVasa <153182387+VitVasa@users.noreply.github.com>
2025-09-03 15:20:55 +0300

    Update main.cpp
    Добавлена функция для сохранения базы данных студентов из файла.

--- main.cpp
+++ main.cpp
@@ -36,6 +36,40 @@ void displayStudents(const std::vector<Student>& database) {
     }
 }
 
+void loadDatabase(std::vector<Student>& database, const std::string& filename) {
+    std::ifstream file(filename);
+    
+    if (!file.is_open()) {
+        std::cout << "Ошибка открытия файла для чтения!\n";
+        return;
+    }
+    
+    
+    std::string line;
+    while (std::getline(file, line)) {
+        std::vector<std::string> fields;
+        std::stringstream ss(line);
+        std::string field;
+        
+        while (std::getline(ss, field, ',')) {
+            fields.push_back(field);
+        }
+        
+        if (fields.size() == 4) {
+            Student student;
+            student.name = fields[0];
+            student.age = std::stoi(fields[1]);
+            student.major = fields[2];
+            student.gpa = std::stod(fields[3]);
+            
+            database.push_back(student);
+        }
+    }
+    
+    file.close();
+    std::cout << "База данных загружена из файла: " << filename << "\n";
+}
+
 int main() {
     std::vector<Student> database;
 
